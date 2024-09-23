159eb941e8f0e3a69297534b60a13665cf55d6e1
Seth Way <way.sethaniel@gmail.com>
2024-09-23 12:37:24 -0400

    it 4. test form

--- cypress/e2e/form_validation.spec.cy.js
+++ cypress/e2e/form_validation.spec.cy.js
@@ -3,9 +3,29 @@ beforeEach(() => {
     statusCode: 200,
     fixture: 'orders',
   }).as('GET_ALL_ORDERS');
+  cy.intercept(
+    'POST',
+    'http://localhost:3001/api/v1/orders',
+    cy.spy().as('CREATE_ORDER')
+  );
   cy.visit('http://localhost:3000');
+  cy.get('button').contains('Submit Order').as('submit');
 });
 
-describe('empty spec', () => {
-  it('passes', () => {});
+describe('Should not submit form if necessary info is not provided', () => {
+  it("shouldn't submit if no name is provided", () => {
+    cy.get('button').contains('beans').click();
+    cy.get('button').contains('steak').click();
+    cy.get('@submit').click();
+    cy.get('@CREATE_ORDER').should('have.not.been.called');
+  });
+  it("shouldn't submit if no ingredients are provided", () => {
+    cy.get('input').type('Seth');
+    cy.get('@submit').click();
+    cy.get('@CREATE_ORDER').should('have.not.been.called');
+  });
+  it("shouldn't submit if neither are provided", () => {
+    cy.get('@submit').click();
+    cy.get('@CREATE_ORDER').should('have.not.been.called');
+  });
 });
--- cypress/e2e/new_order.spec.cy.js
+++ cypress/e2e/new_order.spec.cy.js
@@ -3,11 +3,21 @@ beforeEach(() => {
     statusCode: 200,
     fixture: 'orders',
   }).as('GET_ALL_ORDERS');
+  cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
+    statusCode: 201,
+    fixture: 'order',
+  }).as('POST_NEW_ORDER');
   cy.visit('http://localhost:3000');
 });
 
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('New order form should create new order', () => {
+  it('create new order on form submit', () => {
+    cy.get('input').type('Seth')
+    cy.get('.ingredient-btn').contains('beans').click();
+    cy.get('.ingredient-btn').contains('jalapenos').click();
+    cy.get('button').contains('Submit Order').click();
+    cy.get('section').find('div.order').should('have.length', 4);
+    cy.get('div.order:last-child').find('h3').should('contain', 'Seth');
+    cy.get('div.order:last-child').find('ul.ingredient-list > li').should('have.length', 2);
+  });
+});
